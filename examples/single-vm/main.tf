62741370aa521c5f34c36b910ec9dc3ebcb63f51
Bartek Moczulski <12998104+bartekmo@users.noreply.github.com>
2024-01-17 18:29:27 +0100

    fix formatting

--- examples/single-vm/main.tf
+++ examples/single-vm/main.tf
@@ -1,34 +1,31 @@
 module "fgtimage" {
-    source = "../.."
-    ver    = "7.4.1"
+  source = "../.."
+  ver    = "7.4.1"
+  lic    = "byol"
 }
 
 data "google_compute_default_service_account" "default" {
 }
 
 resource "google_compute_instance" "fgt" {
-    name           = "fortigate-vm"
-    zone           = "us-central1-b"
-    machine_type   = "e2-standard-2"
-    can_ip_forward = true
+  name           = "fortigate-vm"
+  zone           = "us-central1-b"
+  machine_type   = "e2-standard-2"
+  can_ip_forward = true
 
-    boot_disk {
-      initialize_params {
-        image      = module.fgtimage.self_link
-      }
+  boot_disk {
+    initialize_params {
+      image = module.fgtimage.self_link
     }
+  }
 
-    network_interface {
-      subnetwork   = "external"
-      access_config {}
-    }
+  network_interface {
+    subnetwork = "default"
+    access_config {}
+  }
 
-    network_interface {
-      subnetwork   = "internal"
-    }
-
-    service_account {
-      email        = data.google_compute_default_service_account.default.email
-      scopes       = ["cloud-platform"]
-    }
+  service_account {
+    email  = data.google_compute_default_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
 }
